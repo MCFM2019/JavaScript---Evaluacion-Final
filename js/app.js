c449b780f9d8888c57996b105f9d32bab876113d
Maria Cristina <mcfeijoo2019@gmail.com>
2019-08-13 16:11:52 -0500

    Calculo de operaciones parte 1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Se crea la función principal
 var Calculadora={
-  iniciar:function(){
-    var numAnterior,operacion;
+  numAnterior: '',
+  operacion: '',
+  teclaPrevia: '',
 
+  iniciar:function(){
     var numPantalla=document.getElementById("display");
 
     var reinicio=document.getElementById("on");
@@ -26,28 +28,28 @@ var Calculadora={
     var ocho=document.getElementById("8");
     var nueve=document.getElementById("9");
 
-    this.presionaTecla(reinicio,numPantalla,numAnterior,operacion);
-    this.presionaTecla(signo,numPantalla,numAnterior,operacion);
-    this.presionaTecla(raiz,numPantalla,numAnterior,operacion);
-    this.presionaTecla(dividir,numPantalla,numAnterior,operacion);
-    this.presionaTecla(multiplicar,numPantalla,numAnterior,operacion);
-    this.presionaTecla(menos,numPantalla,numAnterior,operacion);
-    this.presionaTecla(mas,numPantalla,numAnterior,operacion);
-    this.presionaTecla(igual,numPantalla,numAnterior,operacion);
-    this.presionaTecla(punto,numPantalla,numAnterior,operacion);
+    this.presionaTecla(reinicio,numPantalla);
+    this.presionaTecla(signo,numPantalla);
+    this.presionaTecla(raiz,numPantalla);
+    this.presionaTecla(dividir,numPantalla);
+    this.presionaTecla(multiplicar,numPantalla);
+    this.presionaTecla(menos,numPantalla);
+    this.presionaTecla(mas,numPantalla);
+    this.presionaTecla(igual,numPantalla);
+    this.presionaTecla(punto,numPantalla);
 
-    this.presionaTecla(cero,numPantalla,numAnterior,operacion);
-    this.presionaTecla(uno,numPantalla,numAnterior,operacion);
-    this.presionaTecla(dos,numPantalla,numAnterior,operacion);
-    this.presionaTecla(tres,numPantalla,numAnterior,operacion);
-    this.presionaTecla(cuatro,numPantalla,numAnterior,operacion);
-    this.presionaTecla(cinco,numPantalla,numAnterior,operacion);
-    this.presionaTecla(seis,numPantalla,numAnterior,operacion);
-    this.presionaTecla(siete,numPantalla,numAnterior,operacion);
-    this.presionaTecla(ocho,numPantalla,numAnterior,operacion);
-    this.presionaTecla(nueve,numPantalla,numAnterior,operacion);
+    this.presionaTecla(cero,numPantalla);
+    this.presionaTecla(uno,numPantalla);
+    this.presionaTecla(dos,numPantalla);
+    this.presionaTecla(tres,numPantalla);
+    this.presionaTecla(cuatro,numPantalla);
+    this.presionaTecla(cinco,numPantalla);
+    this.presionaTecla(seis,numPantalla);
+    this.presionaTecla(siete,numPantalla);
+    this.presionaTecla(ocho,numPantalla);
+    this.presionaTecla(nueve,numPantalla);
   },
-  presionaTecla:function (tecla,numPantalla,numAnterior,operacion){
+  presionaTecla:function (tecla,numPantalla){
     tecla.addEventListener('mousedown',function(){
       tecla.style.transform='scale(0.85,0.85)';
     })
@@ -61,65 +63,87 @@ var Calculadora={
       switch (teclaPresionada){
         case 'on':
           numPantalla.innerHTML='0';
+          Calculadora.teclaPrevia='';
+          Calculadora.operacion='';
+          Calculadora.teclaPrevia='';
           break;
         case 'sign':
           if (numeroEnPantalla!='0'){
             numPantalla.innerHTML=parseFloat(numeroEnPantalla)*-1;
           }
+          Calculadora.teclaPrevia='';
           break;
         case 'raiz':
           //El botón de raiz cuadrada no hace nada
+          Calculadora.teclaPrevia='';
           break;
         case 'dividido':
-          Calculadora.guardaValoresDeOperacion(numAnterior,numPantalla,teclaPresionada);
+          Calculadora.guardaValoresDeOperacion(numPantalla,teclaPresionada);
+          Calculadora.teclaPrevia='';
           break;
         case 'por':
-          Calculadora.guardaValoresDeOperacion(numAnterior,numPantalla,teclaPresionada);
+          Calculadora.guardaValoresDeOperacion(numPantalla,teclaPresionada);
+          Calculadora.teclaPrevia='';
           break;
         case 'menos':
-          Calculadora.guardaValoresDeOperacion(numAnterior,numPantalla,teclaPresionada);
+          Calculadora.guardaValoresDeOperacion(numPantalla,teclaPresionada);
+          Calculadora.teclaPrevia='';
           break;
         case 'mas':
-          Calculadora.guardaValoresDeOperacion(numAnterior,numPantalla,teclaPresionada);
+          Calculadora.guardaValoresDeOperacion(numPantalla,teclaPresionada);
+          Calculadora.teclaPrevia='';
           break;
         case 'igual':
-
+          console.log('tecla antes:'+Calculadora.teclaPrevia);
+          Calculadora.CalculaResultadoDeLaOperacion(numPantalla);
+          Calculadora.teclaPrevia=teclaPresionada;
+          console.log('tecla desupes:'+Calculadora.teclaPrevia);
           break;
         case 'punto':
-
+          Calculadora.AgregarPuntoAlNumeroEnPantalla(numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '0':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '1':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '2':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '3':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '4':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '5':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '6':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '7':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '8':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         case '9':
           Calculadora.muestraNumeroEnPantalla(teclaPresionada,numPantalla);
+          Calculadora.teclaPrevia='';
           break;
         default:
-
           break;
       }
     })
@@ -134,10 +158,91 @@ var Calculadora={
       }
     }
   },
-  guardaValoresDeOperacion:function(numAnterior,numPantalla,teclaPresionada){
-    numAnterior=numPantalla.innerHTML;
-    operacion=teclaPresionada;
+  guardaValoresDeOperacion:function(numPantalla,teclaPresionada){
+    Calculadora.numAnterior=numPantalla.innerHTML;
+    Calculadora.operacion=teclaPresionada;
     numPantalla.innerHTML='';
+  },
+  AgregarPuntoAlNumeroEnPantalla:function(numPantalla){
+    //El numero no tiene punto
+    if ((numPantalla.innerHTML.indexOf('.')<0) && (numPantalla.innerHTML!='')){
+      numPantalla.innerHTML+='.';
+    }
+  },
+  CalculaResultadoDeLaOperacion:function(numPantalla){
+    if (numPantalla.innerHTML!='0'){
+      console.log('Dentro:'+Calculadora.teclaPrevia);
+      var numAntesDelResultado=numPantalla.innerHTML;
+      var numOperacionFinal;
+
+      numOperacionFinal=this.devuelveResultadoDeOperacion(numPantalla);
+      numPantalla.innerHTML=numOperacionFinal;
+
+      //Para que se guarde el ultimo numero y poder hacer operaciones consecutivas
+      if (numAntesDelResultado!='' && Calculadora.teclaPrevia==''){
+        Calculadora.numAnterior=numAntesDelResultado;
+      }
+    }
+  },
+  devuelveResultadoDeOperacion:function(numPantalla){
+    var numOper;
+    switch(Calculadora.operacion){
+      case 'dividido':
+        if (Calculadora.operacion!='' && Calculadora.numAnterior!=''){
+          if (numPantalla.innerHTML!=''){
+            numOper=parseFloat(Calculadora.numAnterior)/parseFloat(numPantalla.innerHTML);
+            this.validaNumeroAOchoDigitos(numOper);
+          }
+          else{
+            numOper=Calculadora.numAnterior;
+          }
+          return numOper;
+        }
+        break;
+      case 'por':
+        if (Calculadora.operacion!='' && Calculadora.numAnterior!=''){
+          if (numPantalla.innerHTML!=''){
+            numOper=parseFloat(Calculadora.numAnterior)*parseFloat(numPantalla.innerHTML);
+            this.validaNumeroAOchoDigitos(numOper);
+          }
+          else{
+            numOper=Calculadora.numAnterior='';;
+          }
+          return numOper;
+        }
+        break;
+      case 'menos':
+        if (Calculadora.operacion!='' && Calculadora.numAnterior!=''){
+          if (numPantalla.innerHTML!=''){
+            numOper=parseFloat(Calculadora.numAnterior)-parseFloat(numPantalla.innerHTML);
+            this.validaNumeroAOchoDigitos(numOper);
+          }
+          else{
+            numOper=Calculadora.numAnterior;
+          }
+          return numOper;
+        }
+        break;
+      case 'mas':
+      if (Calculadora.operacion!='' && Calculadora.numAnterior!=''){
+          if (numPantalla.innerHTML!=''){
+            numOper=parseFloat(Calculadora.numAnterior)+parseFloat(numPantalla.innerHTML);
+            this.validaNumeroAOchoDigitos(numOper);
+          }
+          else{
+            numOper=Calculadora.numAnterior;
+          }
+          return numOper;
+        }
+        break;
+      default:
+        break;
+    }
+  },
+  validaNumeroAOchoDigitos:function(numOper){
+    if (numOper.length>=8){
+      console.log('Se excedio el numero de digitos')
+    }
   }
 }
 
